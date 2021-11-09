95f1a672ecea6541abedcf85f07fecd6d9eebcaa
nmyshuniaiev <nmyshuniaiev@gmail.com>
2021-11-08 22:27:40 -0500

    Tablica

--- public/script.js
+++ public/script.js
@@ -1,23 +1,83 @@
+$( document ).ready(function() {
+    $("#list-group").hide();
+});
+
 const TruckOnClick = () => {
-    $(".home-page").fadeOut();
-    $(".third-page").fadeOut();
+    $(".home-page").hide();
+    $(".third-page").hide();
     $(".second-page").fadeIn(200);
 }
 
 const TrailerOnClick = () => {
-    $(".home-page").fadeOut();
-    $(".third-page").fadeOut();
+    $(".home-page").hide();
+    $(".third-page").hide();
     $(".second-page").fadeIn(200);
 }
 
 const BackOnClick = () => {
-    $(".second-page").fadeOut();
-    $(".third-page").fadeOut();
+    $(".second-page").hide();
+    $(".third-page").hide();
     $(".home-page").fadeIn(200);
 }
 
 const NextOnClick = () => {
-    $(".second-page").fadeOut();
-    $(".home-page").fadeOut();
+    $(".second-page").hide();
+    $(".home-page").hide();
     $(".third-page").fadeIn(200);
+}
+
+const DropdownButtonOnClick = (name) => {
+    switch (name) {
+        case "Trailer Electronics":
+            document.getElementById("list-group").innerHTML = 
+            `
+                <li class="list-group-item" onclick="ListGroupItemOnClick()">REPAIRED  LIGHT<span style="float: right;">→</span></li>
+                <li class="list-group-item" onclick="ListGroupItemOnClick()">REPLACED CABLE<span style="float: right;">→</span></li>
+                <li class="list-group-item" onclick="ListGroupItemOnClick()">Repair Cable<span style="float: right;">→</span></li>
+                <li class="list-group-item" onclick="ListGroupItemOnClick()">REPLACED ABS TRAILER MODULE<span style="float: right;">→</span></li>
+                <li class="list-group-item" onclick="ListGroupItemOnClick()">Replaced ABS sensors<span style="float: right;">→</span></li>
+            `
+            break;
+        case "Trailer Brake":
+            document.getElementById("list-group").innerHTML = 
+            `
+            <ul>
+                <li class="list-group-item" onclick="ListGroupItemOnClick()">Replaced Brakes<span style="float: right;">→</span></li>
+                <li class="list-group-item" onclick="ListGroupItemOnClick()">Replaced Drams<span style="float: right;">→</span></li>
+                <li class="list-group-item" onclick="ListGroupItemOnClick()">REPLACED SLACK ADJUSTER<span style="float: right;">→</span></li>
+                <li class="list-group-item" onclick="ListGroupItemOnClick()">REPLACED scam<span style="float: right;">→</span></li>
+                <li class="list-group-item" onclick="ListGroupItemOnClick()">REPLACED BRAKE CHAMBER<span style="float: right;">→</span></li>
+            </ul>
+            `
+            break;
+        case "Air System of the Trailer":
+            document.getElementById("list-group").innerHTML = 
+            `
+                <li class="list-group-item" onclick="ListGroupItemOnClick()">REPAIRED  LIGHT<span style="float: right;">→</span></li>
+                <li class="list-group-item" onclick="ListGroupItemOnClick()">REPLACED CABLE<span style="float: right;">→</span></li>
+                <li class="list-group-item" onclick="ListGroupItemOnClick()">Repair Cable<span style="float: right;">→</span></li>
+                <li class="list-group-item" onclick="ListGroupItemOnClick()">REPLACED ABS TRAILER MODULE<span style="float: right;">→</span></li>
+                <li class="list-group-item" onclick="ListGroupItemOnClick()">Replaced ABS sensors<span style="float: right;">→</span></li>
+            `
+            break;
+        case "Engine":
+
+            break;
+        case "Trailer SUSPENSION":
+
+            break;
+        case "Trailer Repair":
+
+            break;
+                                                                
+        default:
+            break;
+    }
+    $(".dropdown-buttons").hide();
+    $("#list-group").fadeIn(200);
+}
+
+const ListGroupItemOnClick = () => {
+    $("#list-group").hide();
+    $(".dropdown-buttons").fadeIn(200);
 }
