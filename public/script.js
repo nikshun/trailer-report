5adbd85a1a41c24c818f5c594aeb96d53efd4501
nmyshuniaiev <nmyshuniaiev@gmail.com>
2021-11-09 00:43:19 -0500

    Added boxes deletion

--- public/script.js
+++ public/script.js
@@ -1,3 +1,4 @@
+var boxNames = []
 
 $( document ).ready(function() {
     $("#list-group").hide();
@@ -27,47 +28,89 @@ const NextOnClick = () => {
     $(".third-page").fadeIn(200);
 }
 
-const ListGroupItemOnClick = () => {
+const ListGroupItemOnClick = (name) => {
     $("#list-group").hide();
     $(".dropdown-buttons").fadeIn(200);
+    boxNames.push(name)
+    ReRenderBoxes()
 }
+
+const DeleteBoxOnClick = (name) => {
+    $("#list-group").hide();
+    $(".dropdown-buttons").fadeIn(200);
+    boxNames = boxNames.filter(item => item != name)
+    ReRenderBoxes()
+}
+
+const ReRenderBoxes = () => {
+    var str = ""
+    boxNames.forEach((nameStr)=>{
+        str += 
+        `
+            <div class="trailer-report-box">
+              <h4 style="float: left;">
+                
+              ` + nameStr + `
+              </h4>
+              <input type="text" class="form-control" id="exampleInputEmail1">
+
+              <div class="form-check" id="exampleCheckID4">
+                <input type="checkbox" class="form-check-input checkbox" id="exampleCheck4">
+                <label class="form-check-label" for="exampleCheck4">Used</label>
+              </div>
+              
+              <button type="button" style="float: right; margin-top: 10px;" class="btn btn-outline-danger" onclick="DeleteBoxOnClick('` + nameStr + `')">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                  <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                </svg>
+                Button
+              </button>
+              
+            </div>
+        `
+    })
+    document.getElementById("right-sidebar").innerHTML = str
+    $("#right-sidebar").fadeIn(200);
+}
+
 
 const DropdownButtonOnClick = (name) => {
     switch (name) {
         case "Trailer Electronics":
             document.getElementById("list-group").innerHTML = 
             `
-                <li class="list-group-item" onclick="ListGroupItemOnClick()">REPAIRED  LIGHT<span style="float: right;">→</span></li>
-                <li class="list-group-item" onclick="ListGroupItemOnClick()">REPLACED CABLE<span style="float: right;">→</span></li>
-                <li class="list-group-item" onclick="ListGroupItemOnClick()">Repair Cable<span style="float: right;">→</span></li>
-                <li class="list-group-item" onclick="ListGroupItemOnClick()">REPLACED ABS TRAILER MODULE<span style="float: right;">→</span></li>
-                <li class="list-group-item" onclick="ListGroupItemOnClick()">Replaced ABS sensors<span style="float: right;">→</span></li>
+                <li class="list-group-item" onclick="ListGroupItemOnClick('REPAIRED  LIGHT')">REPAIRED  LIGHT<span style="float: right;">→</span></li>
+                <li class="list-group-item" onclick="ListGroupItemOnClick('REPLACED CABLE')">REPLACED CABLE<span style="float: right;">→</span></li>
+                <li class="list-group-item" onclick="ListGroupItemOnClick('Repair Cable')">Repair Cable<span style="float: right;">→</span></li>
+                <li class="list-group-item" onclick="ListGroupItemOnClick('REPLACED ABS TRAILER MODULE')">REPLACED ABS TRAILER MODULE<span style="float: right;">→</span></li>
+                <li class="list-group-item" onclick="ListGroupItemOnClick('Replaced ABS sensors')">Replaced ABS sensors<span style="float: right;">→</span></li>
             `
             break;
         case "Trailer Brake":
             document.getElementById("list-group").innerHTML = 
             `
             <ul>
-                <li class="list-group-item" onclick="ListGroupItemOnClick()">Replaced Brakes<span style="float: right;">→</span></li>
-                <li class="list-group-item" onclick="ListGroupItemOnClick()">Replaced Drams<span style="float: right;">→</span></li>
-                <li class="list-group-item" onclick="ListGroupItemOnClick()">REPLACED SLACK ADJUSTER<span style="float: right;">→</span></li>
-                <li class="list-group-item" onclick="ListGroupItemOnClick()">REPLACED scam<span style="float: right;">→</span></li>
-                <li class="list-group-item" onclick="ListGroupItemOnClick()">REPLACED BRAKE CHAMBER<span style="float: right;">→</span></li>
+                <li class="list-group-item" onclick="ListGroupItemOnClick('Replaced Brakes')">Replaced Brakes<span style="float: right;">→</span></li>
+                <li class="list-group-item" onclick="ListGroupItemOnClick('Replaced Drams')">Replaced Drams<span style="float: right;">→</span></li>
+                <li class="list-group-item" onclick="ListGroupItemOnClick('REPLACED SLACK ADJUSTER')">REPLACED SLACK ADJUSTER<span style="float: right;">→</span></li>
+                <li class="list-group-item" onclick="ListGroupItemOnClick('REPLACED scam')">REPLACED scam<span style="float: right;">→</span></li>
+                <li class="list-group-item" onclick="ListGroupItemOnClick('REPLACED BRAKE CHAMBER')">REPLACED BRAKE CHAMBER<span style="float: right;">→</span></li>
             </ul>
             `
             break;
         case "Air System of the Trailer":
             document.getElementById("list-group").innerHTML = 
             `
-                <li class="list-group-item" onclick="ListGroupItemOnClick()">REPLACED AIR LINE<span style="float: right;">→</span></li>
-                <li class="list-group-item" onclick="ListGroupItemOnClick()">REPLACED AIR FITTING<span style="float: right;">→</span></li>
-                <li class="list-group-item" onclick="ListGroupItemOnClick()">REPLACED AIRBAG<span style="float: right;">→</span></li>
-                <li class="list-group-item" onclick="ListGroupItemOnClick()">REPLACED BRAKE VALVE<span style="float: right;">→</span></li>
-                <li class="list-group-item" onclick="ListGroupItemOnClick()">REPLACED PRESSURE PROTECTION VALVE<span style="float: right;">→</span></li>
-                <li class="list-group-item" onclick="ListGroupItemOnClick()">REPLACED AIR TANK<span style="float: right;">→</span></li>
-                <li class="list-group-item" onclick="ListGroupItemOnClick()">REPLACE LEVELING VALVE<span style="float: right;">→</span></li>
-                <li class="list-group-item" onclick="ListGroupItemOnClick()">REPLACED 3 WAY VALVE<span style="float: right;">→</span></li>
-                <li class="list-group-item" onclick="ListGroupItemOnClick()">REPLACED PIN ACTUATOR<span style="float: right;">→</span></li>
+                <li class="list-group-item" onclick="ListGroupItemOnClick('REPLACED AIR LINE)">REPLACED AIR LINE<span style="float: right;">→</span></li>
+                <li class="list-group-item" onclick="ListGroupItemOnClick('REPLACED AIR FITTING)">REPLACED AIR FITTING<span style="float: right;">→</span></li>
+                <li class="list-group-item" onclick="ListGroupItemOnClick('REPLACED AIRBAG)">REPLACED AIRBAG<span style="float: right;">→</span></li>
+                <li class="list-group-item" onclick="ListGroupItemOnClick('REPLACED BRAKE VALVE)">REPLACED BRAKE VALVE<span style="float: right;">→</span></li>
+                <li class="list-group-item" onclick="ListGroupItemOnClick('REPLACED PRESSURE PROTECTION VALVE')">REPLACED PRESSURE PROTECTION VALVE<span style="float: right;">→</span></li>
+                <li class="list-group-item" onclick="ListGroupItemOnClick('REPLACED AIR TANK')">REPLACED AIR TANK<span style="float: right;">→</span></li>
+                <li class="list-group-item" onclick="ListGroupItemOnClick('REPLACE LEVELING VALVE')">REPLACE LEVELING VALVE<span style="float: right;">→</span></li>
+                <li class="list-group-item" onclick="ListGroupItemOnClick('REPLACED 3 WAY VALVE')">REPLACED 3 WAY VALVE<span style="float: right;">→</span></li>
+                <li class="list-group-item" onclick="ListGroupItemOnClick('REPLACED PIN ACTUATOR')">REPLACED PIN ACTUATOR<span style="float: right;">→</span></li>
             `
             break;
         case "Engine":
@@ -132,6 +175,7 @@ const DropdownButtonOnClick = (name) => {
         default:
             break;
     }
+
     $(".dropdown-buttons").hide();
     $("#list-group").fadeIn(200);
 }
